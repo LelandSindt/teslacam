24fc4263658b718ea1bd737c96707c727105f1a8
Leland Sindt <leland@sway.org>
2018-11-02 18:37:57 -0500

    move powermonitor from cron to service.

--- powermonitor.py
+++ powermonitor.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python 
-
-# root's crontab 
-# m h  dom mon dow   command
-#  * *  *   *   *     /home/pi/powermonitor.py
 import pijuice
 import os
+import time
 pj=pijuice.PiJuice(1, 0x14)
 pj.power.SetWakeUpOnCharge(0)
-if pj.status.GetStatus()['data']['powerInput5vIo'] == "NOT_PRESENT":
-  os.system("sudo halt")
+pj.power.SetSystemPowerSwitch(0)
+powerloss = 0
+while True:
+  time.sleep(2)
+  print time.strftime("%Y-%m-%d %H:%M") + " Charge State: " + pj.status.GetStatus()['data']['powerInput5vIo'] + " Charge Percent: " +  str(pj.status.GetChargeLevel()['data']) + " Power Loss Count: " + str(powerloss)
+  if pj.status.GetStatus()['data']['powerInput5vIo'] == "NOT_PRESENT":
+    powerloss = powerloss + 1
+  else:
+    powerloss = 0
+  if powerloss > 10:
+    print("Stopping TeslaCam...")
+    os.system("sudo systemctl stop teslacam")
+    print("Powering Down...")
+    pj.power.SetSystemPowerSwitch(0)
+    pj.power.SetPowerOff(30)
+    os.system("sudo halt")

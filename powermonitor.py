e77c19b15380a2b78ea55572a82197fbc810438a
Leland Sindt <leland@sway.org>
2018-11-02 22:02:22 -0500

    print -> sys.stdout.write/flush

--- powermonitor.py
+++ powermonitor.py
@@ -2,6 +2,7 @@
 import pijuice
 import os
 import time
+import sys
 pj=pijuice.PiJuice(1, 0x14)
 pj.power.SetWakeUpOnCharge(0)
 pj.power.SetSystemPowerSwitch(0)
@@ -9,7 +10,8 @@ powerloss = 0
 sleeptime = 30
 while True:
   time.sleep(int(sleeptime))
-  print time.strftime("%Y-%m-%d %H:%M") + " Charge State: " + pj.status.GetStatus()['data']['powerInput5vIo'] + " Charge Percent: " +  str(pj.status.GetChargeLevel()['data']) + " Power Loss Count: " + str(powerloss)
+  sys.stdout.write(time.strftime("%Y-%m-%d %H:%M") + " Charge State: " + pj.status.GetStatus()['data']['powerInput5vIo'] + " Charge Percent: " +  str(pj.status.GetChargeLevel()['data']) + " Power Loss Count: " + str(powerloss) + "\n")
+  sys.stdout.flush()
   if pj.status.GetStatus()['data']['powerInput5vIo'] == "NOT_PRESENT":
     powerloss = powerloss + 1
     sleeptime = 5
@@ -17,9 +19,11 @@ while True:
     powerloss = 0
     sleeptime = 30
   if powerloss > 10:
-    print("Stopping TeslaCam...")
+    sys.stdout.write("Stopping TeslaCam... \n")
+    sys.stdout.flush()
     os.system("sudo systemctl stop teslacam")
-    print("Powering Down...")
+    sys.stdout.write("Powering Down... \n")
+    sys.stdout.flush()
     pj.power.SetSystemPowerSwitch(0)
     pj.power.SetPowerOff(30)
     os.system("sudo halt")

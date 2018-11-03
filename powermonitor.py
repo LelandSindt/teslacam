64a80cc75d60d5344e8fd014036a90c31f6ac7f0
Leland Sindt <leland@sway.org>
2018-11-02 21:45:37 -0500

    decreased/dynamic sleep time

--- powermonitor.py
+++ powermonitor.py
@@ -6,13 +6,16 @@ pj=pijuice.PiJuice(1, 0x14)
 pj.power.SetWakeUpOnCharge(0)
 pj.power.SetSystemPowerSwitch(0)
 powerloss = 0
+sleeptime = 30
 while True:
-  time.sleep(2)
+  time.sleep(int(sleeptime))
   print time.strftime("%Y-%m-%d %H:%M") + " Charge State: " + pj.status.GetStatus()['data']['powerInput5vIo'] + " Charge Percent: " +  str(pj.status.GetChargeLevel()['data']) + " Power Loss Count: " + str(powerloss)
   if pj.status.GetStatus()['data']['powerInput5vIo'] == "NOT_PRESENT":
     powerloss = powerloss + 1
+    sleeptime = 5
   else:
     powerloss = 0
+    sleeptime = 30
   if powerloss > 10:
     print("Stopping TeslaCam...")
     os.system("sudo systemctl stop teslacam")
